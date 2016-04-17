d6185884dd696e239bf9e8d0283ecc825efbae19
JoinDomain <joindomain@ADS.CPUT.AC.ZA>
2016-04-17 16:21:10 +0200

    Abel, Assignment 6

---
+++ app/src/main/java/Domain/CustomerInterface.java
@@ -0,0 +1,11 @@
+package Domain;
+
+/**
+ * Created by AbelN on 15/04/2016.
+ */
+public interface CustomerInterface {
+
+    int getCredits();
+    CustomerAddress getCustomerAddress();
+    PersonalInformation getPersonalInformation();
+}
